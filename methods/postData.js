28a2d19fb9f09525fd4e9231ef61d099ff4b4a75
claire <claire.chang@starclinic.com.tw>
2022-04-16 13:39:26 +0800

    update

--- methods/postData.js
+++ methods/postData.js
@@ -1,6 +1,33 @@
 const { successHandler, errorHandler } = require('../handler');
 const Post = require('../models/post');
 
+const errorArr = {
+    "errors": {
+        "name": {
+            "name": "ValidatorError",
+            "message": "貼文姓名未填寫",
+            "properties": {
+                "message": "貼文姓名未填寫",
+                "type": "required",
+                "path": "name"
+            },
+            "kind": "required",
+            "path": "name"
+        },
+        "content": {
+            "name": "ValidatorError",
+            "message": "content 未填寫",
+            "properties": {
+                "message": "content 未填寫",
+                "type": "required",
+                "path": "content"
+            },
+            "kind": "required",
+            "path": "content"
+        }
+    }
+}
+
 const postData = async (req, res, body) => {
     req.on('end', async () => {
         try {
@@ -8,7 +35,7 @@ const postData = async (req, res, body) => {
             const newPost = await Post.create(data);
             successHandler(res, '新增成功', newPost);
         } catch(error) {
-            const errorStr = Object.values(error.errors).map(item => item.message).join('、');
+            // const errorStr = Object.values(error.errors).map(item => item.message).join('、');
             errorHandler(res, error);
         }
     });
